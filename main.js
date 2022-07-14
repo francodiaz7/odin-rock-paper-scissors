0443540e509ef6e8f6cfdc96ceb1b780d082d62d
Franco Diaz2 <francoagustindiaz7@gmail.com>
2022-07-14 15:22:21 -0300

    Add function playerPlay

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ function playRound(playerSelection, computerSelection) {
 
 function computerPlay()
 
-function playerPlay()
+function playerPlay() {
+    count = 0
+    playing = true
+    while (playing) {
+        playerInput = prompt('Enter your selection')
+        playerInput = playerInput.toLowerCase()
+        if (playerInput) {
+            // Player hit cancel or entered a empty string
+            alert('Please input an allowed value')
+            count++
+            if (count == 3) {
+                alert('Player has left the match')
+                playing = false
+            }
+            continue
+        } else if (playerInput != "rock" && playerInput != "paper" && playerInput != "scissors") {
+        // Player entered a value that isn't either rock, paper or scissors
+        alert('Enter a valid option')
+        continue
+        } else {
+            return playerInput
+        }
+    }
+} 
+    return playerInput
 
 function game()
